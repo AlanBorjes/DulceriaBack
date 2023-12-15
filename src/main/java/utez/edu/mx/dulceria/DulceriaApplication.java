4eecb2cf006985e61d13e4c0c60db8e6dbb29ae4
obed-hurtado12 <20193tn117@utez.edu.mx>
2023-12-15 14:45:45 -0600

    arreglos
    arreglos

--- src/main/java/utez/edu/mx/dulceria/DulceriaApplication.java
+++ src/main/java/utez/edu/mx/dulceria/DulceriaApplication.java
@@ -1,10 +1,9 @@
 package utez.edu.mx.dulceria;
 
-import org.springframework.context.ApplicationContext;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import utez.edu.mx.dulceria.rol.model.Rol;
-import utez.edu.mx.dulceria.rol.repository.RolRepository;
+
 
 @SpringBootApplication
 public class DulceriaApplication {
